da34d7093227d265a8a24207c9e4688a30799a3c
Aaron Diaz <aaron.diaz@iesdoctorbalmis.com>
2022-05-24 12:49:15 -0700

    cambios populares
    añadido algunos estilos preliminares y algunas funcionalidades a la página de populares.html

--- js/index.js
+++ js/index.js
@@ -1,10 +1,5 @@
 let página=1;
-document.querySelector(".mostrarMas").addEventListener("click", mostrarMas);
 
-async function mostrarMas(){
-    let películasPopulares= await Populares.getMásPopulares(++página);
-    buscaPeliculas(películasPopulares).forEach(item=>item.DibujaPelicula());
-}
 
 document.addEventListener("DOMContentLoaded",async function()
 {
@@ -30,6 +25,33 @@ document.addEventListener("DOMContentLoaded",async function()
     }
 });
 
+document.querySelector(".mostrarMas").addEventListener("click", mostrarMas);
+
+async function mostrarMas(){
+    let películasPopulares= await Populares.getMásPopulares(++página);
+    buscaPeliculas(películasPopulares).forEach(item=>item.DibujaPelicula());
+}
+
+async function mostrarInfo(pelicula){
+    document.querySelector(".offcanvas").innerHTML=
+    "<div class=\"offcanvas-header\">"+
+        "<h5 class=\"offcanvas-title\" id=\"offcanvasExampleLabel\">"+pelicula.querySelector(".titulo").textContent+"</h5>"+
+        "<button type=\"button\" class=\"btn-close\" data-bs-dismiss=\"offcanvas\" aria-label=\"Close\"></button>"+
+    "</div>"+
+    "<div class=\"offcanvas-body\">"+
+        "<div><img width='80%' src=\""+pelicula.querySelector(".imagen").src+"\" alt=\"Poster de "+pelicula.querySelector(".titulo").textContent+"\"/></div>"+
+        "<div>Nota:<br/>"+
+            pelicula.querySelector(".nota").innerHTML+
+        "</div>"+
+        "<div>Sinopsis:<br/>"+
+            pelicula.querySelector(".sinopsis").textContent+
+        "</div>"+
+        "<div>Fecha de estreno:<br/>"+
+            pelicula.querySelector(".fecha").textContent+
+        "</div>"+
+    "</div>";
+}
+
 function buscaPeliculas(películas){
     const p=[];
     for (let i = 0; i < películas.length; i++) {
@@ -39,3 +61,26 @@ function buscaPeliculas(películas){
     return p;
 }
 
+//botón back to top
+let bttboton = document.getElementById("btn-back-to-top");
+window.onscroll = function () {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  if (
+    document.body.scrollTop > 20 ||
+    document.documentElement.scrollTop > 20
+  ) {
+    bttboton.style.display = "block";
+  } else {
+    bttboton.style.display = "none";
+  }
+}
+
+bttboton.addEventListener("click", backToTop);
+
+function backToTop() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
--- js/pelicula.class.js
+++ js/pelicula.class.js
@@ -8,18 +8,19 @@ class Pelicula{
     }
     DibujaPelicula(){
         document.querySelector(".contenido").innerHTML+=
-        "<div class=\"card\" style=\"width: 18rem;\">"+
-            "<img src=\""+IMAGE_BASE+this.poster+"\" class=\"card-img-top\" alt=\"Poster de "+this.titulo+"\">"+
+        "<div class=\"card\" style=\"width: 18rem;\" onclick=\"mostrarInfo(this)\" data-bs-toggle=\"offcanvas\" data-bs-target=\"#offcanvasExample\" aria-controls=\"offcanvasExample\">"+
+            "<img src=\""+IMAGE_BASE+this.poster+"\" class=\"card-img-top imagen\" alt=\"Poster de "+this.titulo+"\">"+
             "<div class=\"card-body\">"+
-                "<h5 class=\"card-title\">"+this.titulo+"</h5>"+
+                "<h5 class=\"card-title titulo\">"+this.titulo+"</h5>"+
             "</div>"+
             "<ul class=\"list-group list-group-flush\">"+
-                    "<li class=\"list-group-item\">"+new Date(this.fecha).toLocaleDateString()+"</li>"+
-                    "<li class=\"list-group-item\">"+
+                    "<li class=\"list-group-item fecha\">"+new Date(this.fecha).toLocaleDateString()+"</li>"+
+                    "<li class=\"list-group-item nota\">"+
                     "<div class=\"progress\">"+
                         "<div class=\"progress-bar\" role=\"progressbar\" style=\"width: "+this.nota*10+"%;\" aria-valuenow=\""+this.nota*10+"\" aria-valuemin=\"0\" aria-valuemax=\"100\">"+this.nota*10+"%</div>"+
                     "</div></li>"+
             "</ul>"+
+            "<div hidden class=\"sinopsis\">"+this.sinopsis+"</div>"+
         "</div>";
     }
     DibujaIndex(){
