fadbbb8b8f02a7c6aac45ed916291adb73e68d6c
diazAaron <93204053+diazAaron@users.noreply.github.com>
2022-05-31 05:36:01 -0700

    Update cartelera.class.js

--- js/cartelera.class.js
+++ js/cartelera.class.js
@@ -2,7 +2,7 @@
 class Cartelera
 {
     static getNuevasPeliculas = () => {
-        const url = `${API_BASE_URL}movie/now_playing?api_key=${API_KEY}&language=es-ES&page=1`
+        const url = `${API_BASE_URL}/3/movie/now_playing?api_key=${API_KEY}&language=es-ES&page=1`
     
 
         return fetch(url)
